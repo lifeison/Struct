48dc6a2b327c42878b85f606ba186b9b87b4c183
lifeison <32013650+lifeison@users.noreply.github.com>
2017-11-02 21:10:43 +0300

    Delete stacktest.h

--- stacktest.h
+++
@@ -1,7 +0,0 @@
-/**
- * File contains some test values
- */
-
-#define MEM_CHUNK_TEST_VALUE 27000
-#define RATIO_TEST_VALUE 27.000
-#define CHUNK_DUMP_TEST_VALUE "ALL WORK AND NO PLAY MAKES JACK A DULL BOY\0"
